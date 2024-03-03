583ebf9cbfcdebf7844ab7a49f6a026474a2a6f9
Piero Dotti <progiemmeh@gmail.com>
2024-03-03 19:11:14 +0100

    fix(gitpod): more settings tuning

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module TestRails
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.hosts << /.*/ # For gitpod
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
